bcce7255211749b12f5ee763c315ab441b98bcec
Jack D. You <gmyou@youui-macbook-air.local>
2016-09-19 12:53:38 +0900

    Initial Commit

---
+++ weddingdeal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weddingdeal
+//
+//  Created by You Gun-Myoung on 2016. 9. 19..
+//  Copyright © 2016년 You Gun-Myoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
